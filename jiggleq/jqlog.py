cecb1495be14f8f1113a620ef6cade1b899497ba
James Mistry <james.mistry@gmail.com>
2017-07-08 14:15:21 +1200

    Simplified logging helper

--- jiggleq/jqlog.py
+++ jiggleq/jqlog.py
@@ -3,38 +3,10 @@ import sys
 
 def get(obj):
     """
-    Get a logger based on the calling object's namespace.
+    Get a logger for JiggleQ.
 
-    @param obj: The calling object from which the logger name will be derived
+    @param obj: The calling object.
     """
 
-    return logging.getLogger("jiggleq.{0}".format(obj.__class__.__name__))
+    return logging.getLogger("jiggleq")
 
-#class JqLogger(object):
-#    """!
-#    Singleton for constructing the default JiggleQ logger object.
-#    """
-
-#    ## Logger instance
-#    instance = None
-
-#   @staticmethod
-#    def get(obj):
-#        """
-#        Get a logger based on the calling object's namespace.
-#
-#        @param obj: The calling object from which the logger name will be derived
-#        """
-
-#        return logging.getLogger("{0}.{1}".format(obj.__module__, obj.__class__.__name__))
-
-        #if (JqLogger.instance is None):
-        #    JqLogger.instance = logging.getLogger("jiggleq")
-        #    JqLogger.instance.setLevel(logging.ERROR)
-        #    ch = logging.StreamHandler(sys.stdout)
-        #    formatter = logging.Formatter("%(levelname)s\t%(asctime)s\t%(filename)s:%(lineno)s] %(message)s")
-        #    ch.setFormatter(formatter)
-            
-        #    JqLogger.instance.addHandler(ch)
-        
-        #return JqLogger.instance
