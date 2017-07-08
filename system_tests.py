7b6ec5d1e98619ef590f1b0b5df1160b53acbe9f
James Mistry <james.mistry@gmail.com>
2017-07-08 14:17:36 +1200

    Updated system tests and runner to use unittest module

--- system_tests.py
+++ system_tests.py
@@ -1,19 +1,16 @@
-import sys
+from __future__ import print_function
+import unittest
+import os
+import logging
+import tests.system.perf_test
 
-import tests.system.test_elastic
-import tests.system.test_perf
+if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)s\t%(asctime)s\t%(filename)s:%(lineno)s] %(message)s', level=logging.INFO)
+    logging.getLogger('elasticsearch').setLevel(logging.ERROR)
+    scriptDir = os.path.dirname(os.path.abspath(__file__))
+    testsuite = unittest.TestLoader().discover("{0}/tests/system".format(scriptDir), pattern="*_test.py")
+    unittest.TextTestRunner(verbosity=1).run(testsuite)
 
-all_tests_passed = True
+    if (not tests.system.perf_test.run()):
+        sys.exit(1)
 
-if (not tests.system.test_elastic.run()):
-    all_tests_passed = False
-
-if (not tests.system.test_perf.run()):
-    all_tests_passed = False
-
-if (not all_tests_passed):
-    print("One or more tests failed :-(")
-    sys.exit(1)
-else:
-    print("All tests passed :-)")
-    sys.exit(0)
