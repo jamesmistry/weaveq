b978d4cc4872f8d02f69435619cf61621375cc27
James Mistry <james.mistry@gmail.com>
2017-07-08 14:13:50 +1200

    Added more exception classes

--- jiggleq/jqexception.py
+++ jiggleq/jqexception.py
@@ -9,3 +9,27 @@ class JiggleQError(Exception):
         @param message string: Error description
         """
         super(JiggleQError, self).__init__(message)
+
+class TextQueryCompileError(JiggleQError):
+    """!
+    Exception thrown when a text query fails to compile.
+    """
+    def __init__(self, message):
+        """!
+        Constructor.
+
+        @param message string: Error description
+        """
+        super(TextQueryCompileError, self).__init__(message)
+
+class ConfigurationError(JiggleQError):
+    """!
+    Exception thrown when a configuration can't be loaded.
+    """
+    def __init__(self, message):
+        """!
+        Constructor.
+
+        @param message string: Error description
+        """
+        super(ConfigurationError, self).__init__(message)
