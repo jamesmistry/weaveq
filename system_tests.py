b1eae1c9bff344f8082036439d5a30954d70ce48
James Mistry <james.mistry@gmail.com>
2017-07-25 21:43:20 +1200

    Prevented logging in system test

--- system_tests.py
+++ system_tests.py
@@ -10,7 +10,6 @@ import tests.system.perf_test
 if __name__ == "__main__":
     print("** Test success is exclusively determined by exit code: PASS (0), FAIL (!=0) **")
 
-    logging.basicConfig(format='%(levelname)s\t%(asctime)s\t%(filename)s:%(lineno)s] %(message)s', level=logging.INFO)
     logging.getLogger('elasticsearch').setLevel(logging.ERROR)
     scriptDir = os.path.dirname(os.path.abspath(__file__))
     testsuite = unittest.TestLoader().discover("{0}/tests/system".format(scriptDir), pattern="*_test.py")
